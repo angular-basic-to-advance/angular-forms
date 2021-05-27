2588822dc47bc61b89ac367e300f07082e6e9ad2
saroj-kumar-barik <sarojkumarbarik1143@gmail.com>
2021-05-27 13:30:29 +0530

    angular forms | Template driven | without validators

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('f') myform
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form:NgForm){
+  //   console.log("submitted!");
+  //   console.log(form);
+  // }
+  
+  // Another approach
+  onSubmit(form:NgForm){
+    console.log("submitted!");
+    console.log(this.myform);
+  }
+}
